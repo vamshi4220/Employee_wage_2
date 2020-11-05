c12ab5973ebc56c6122127d8e62148d14ee1572e
vamshi4220 <ud75281@gmail.com>
2020-11-05 21:57:23 +0530

    modified: added cases

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,27 @@
 public class EmployeeWage {
-   public static void main(String[] args) {
-      int IS_PART_TIME=1;
-      int IS_FULL_TIME = 2;
-      int EMP_RATE_PER_HOUR = 20;
-      int empHrs = 0;
-      int empWage = 0;
-      double empCheck = Math.floor(Math.random() * 10) % 2;
-      if (empCheck == IS_FULL_TIME) {
-          System.out.println("Employee is Present");
-          empHrs = 8;
-          }
-      else if{
-          System.out.println("Employee is Abscent");
-          empHrs = 0;
-          }
+       public static final int IS_PART_TIME = 1;
+       public static final int IS_FULL_TIME = 2;
+       public static final int EMP_RATE_PER_HOUR = 20;
+
+      public static void main(String[] args) {
+           int empHrs = 0;
+           int empWage = 0;
+      int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+          switch (empCheck) {
+           case IS_PART_TIME:
+           System.out.println("Employee is Present");
+           empHrs = 4;
+           break;
+           case IS_FULL_TIME:
+           System.out.println("Employee is Present");
+           empHrs = 8;
+           break;
+           default:
+           System.out.println("Employee is Absent");
+           empHrs = 0;
+         }
           empWage = empHrs * EMP_RATE_PER_HOUR;
           System.out.println("Emp Wage : " + empWage);
     }
-
 }
+
