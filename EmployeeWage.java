b20537b7363689db8340cccba92e52d942487541
vamshi4220 <ud75281@gmail.com>
2020-11-05 22:21:45 +0530

    monthly employee wage added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,13 @@ public class EmployeeWage {
        public static final int IS_PART_TIME = 1;
        public static final int IS_FULL_TIME = 2;
        public static final int EMP_RATE_PER_HOUR = 20;
+       public static final int NUM_OF_WORKING_DAYS = 20;
 
       public static void main(String[] args) {
            int empHrs = 0;
            int empWage = 0;
+           int totalEmpWage = 0;
+      for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
       int empCheck = (int) Math.floor(Math.random() * 10) % 3;
           switch (empCheck) {
            case IS_PART_TIME:
@@ -20,8 +23,10 @@ public class EmployeeWage {
            System.out.println("Employee is Absent");
            empHrs = 0;
          }
-          empWage = empHrs * EMP_RATE_PER_HOUR;
-          System.out.println("Emp Wage : " + empWage);
+           empWage = empHrs * EMP_RATE_PER_HOUR;
+	   totalEmpWage += empWage;
+           System.out.println("Emp Wage : " + empWage);
+         }
+          System.out.println("Total Emp Wage : " + totalEmpWage);
     }
 }
-
